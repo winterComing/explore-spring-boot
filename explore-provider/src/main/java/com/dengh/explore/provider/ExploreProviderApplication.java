a8298970f175493f46382c33730b293e52fdf739
denghao <dengh20@chinaunicom.cn>
2019-06-25 15:41:30 +0800

    add annoation

--- explore-provider/src/main/java/com/dengh/explore/provider/ExploreProviderApplication.java
+++ explore-provider/src/main/java/com/dengh/explore/provider/ExploreProviderApplication.java
@@ -1,4 +1,12 @@
 package com.dengh.explore.provider;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ExploreProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreProviderApplication.class, args);
+    }
 }
--- explore-web/src/main/java/com/dengh/explore/web/ExploreWebApplication.java
+++ explore-web/src/main/java/com/dengh/explore/web/ExploreWebApplication.java
@@ -1,4 +1,4 @@
-package com.dengh.exploreweb;
+package com.dengh.explore.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
